4c9b8fbae3539f6e894930b8de1edad4ae9a6852
314programs <88335174+314programs@users.noreply.github.com>
2022-05-10 15:11:53 +0800

    Update Q1182.cpp

--- Q1182.cpp
+++ Q1182.cpp
@@ -1,3 +1,4 @@
+//56ms bitmasking
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -31,4 +32,41 @@ int main(void){
     cout << count_;
     
 }
+
+//4ms recursion
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> store_list;
+int list_size, wanted_num;
+int ans = 0;
+
+void backtrack_(vector<int> & numbers, int idx, int count_){
+    if(idx == list_size){
+        if(count_ == wanted_num) ans += 1;
+        return;
+    }
+
+    backtrack_(numbers, idx+1, count_ + numbers[idx]);
+    backtrack_(numbers, idx+1, count_);
+
+}
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    
+    cin >> list_size >> wanted_num;
+
+    vector<int> num_list(list_size);
+    for(int i = 0; i < list_size; i++){
+        cin >> num_list[i];
+    }
+
+    backtrack_(num_list, 0, 0);
+    if(wanted_num == 0) ans -= 1;
+    cout << ans;
+
+}
+
 
