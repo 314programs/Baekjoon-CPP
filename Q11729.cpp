f99d8eda62e1b00eaf4c84a787d4aed8128dcffb
314programs <88335174+314programs@users.noreply.github.com>
2022-06-13 21:25:06 +0800

    Update Q11729.cpp

--- Q11729.cpp
+++ Q11729.cpp
@@ -1,12 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//Label the 3 places A, B, C
+//All circles are placed at A in the beginning, they need to be placed at C at the end
 void solve(int disks, int from, int to){
     if(disks == 0) return;
-    //Move the top disks first
+    //Move the top disks from A to B
     solve(disks-1, from, 6-from-to);
+    //Move current disk to C
     cout << from << " " << to << "\n";
-    //Move back the top disks
+    //Move the other disks from B to C
     solve(disks-1, 6-from-to, to);
 }
 
