53f1a1471bd65e0fb54b9bf5560c1fcf9585942d
314programs <88335174+314programs@users.noreply.github.com>
2022-06-17 14:42:59 +0800

    Create Q8545.cpp

---
+++ Q8545.cpp
@@ -0,0 +1,13 @@
+//Over 2 hours of doing hair in Korea made me tired
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    string word;
+    cin >> word;
+    reverse(word.begin(), word.end());
+    cout << word;
+}
