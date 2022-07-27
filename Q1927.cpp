7e7b296436b75975d3d7577146628fb38465bc09
314programs <88335174+314programs@users.noreply.github.com>
2022-07-27 11:34:19 +0800

    Create Q1927.cpp

---
+++ Q1927.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+    
+    int num_cnt;
+    cin >> num_cnt;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+    while(num_cnt--){
+        int num;
+        cin >> num;
+        if(num == 0){
+            if(pq.empty()) cout << "0\n";
+            else{
+                cout << pq.top() << "\n";
+                pq.pop();
+            }
+        }
+        else{
+            pq.push(num);
+        }
+    }
+
+}
