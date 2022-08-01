02b8fe46116612f61566a012cb87eeef5f64f06d
314programs <88335174+314programs@users.noreply.github.com>
2022-08-01 14:41:14 +0800

    Update KMP.cpp

--- KMP.cpp
+++ KMP.cpp
@@ -1,15 +1,27 @@
-vector<int> pre_process(string s){
-    int s_len = s.size();
-    vector<int> pi(s_len);
+vector<int> pre_processing(string p){
+    //Vector pi, is for measuring fail element
+    //The vector pi records when prefix = suffix, and the number is the length of each
+    //Here is a fail vector of AABABAAB
+    //0 1 0 1 0 1 2 3
+    //First index A is repeated, then at the 3rd index prefix = suffix for 1 length
+    //At the end, AAB(prefix) == AAB(suffix) with length of 3.
+    
+    int m = p.size();
+    vector<int> pi(m);
     pi[0] = 0;
     int j = 0;
+    
+    for(int i = 1; i < m; i++){
+        //j is the longest length of prefix
+        //Keep descreasing j until p[i] == p[j] or it cannot be decreased anymore
+        while(j > 0 && p[i] != p[j]) j = pi[j-1];
 
-    for(int i = 1; i < s_len; i++){
-        while(j > 0 && s[i] != s[j]) j = pi[j-1];
-        if(s[i] == s[j]){
+        //If p[i] and p[j] are equal, increase length of j and record
+        if(p[i] == p[j]){
             pi[i] = j+1;
-            j++;
-        }  
+            j += 1;
+        }
+        //Reset to 0 when it doesn't match
         else{
             pi[i] = 0;
         }
@@ -17,22 +29,26 @@ vector<int> pre_process(string s){
     return pi;
 }
 
-vector<int> match(string s, string p){
-    auto pi = pre_process(p);
-    //Stores index of when p starts inside s
+vector<int> kmp(string s, string p){
+    //Similar to pre_processing but uses the fail vector
+    auto pi = pre_processing(p);
     vector<int> ans;
-    int s_len = s.size(), p_len = p.size();
-    int j = 0;
+    int n = s.size(), m = p.size(), i = 0, j = 0;
 
-    for(int i = 0; i < s_len; i++){
+    for(int i = 0; i < n; i++){
+        //Same reason as previous
         while(j > 0 && s[i] != p[j]) j = pi[j-1];
+
         if(s[i] == p[j]){
-            if(j == p_len - 1){
-                ans.push_back(i-p_len+1);
+            //Means current character is end of string p, meaning that the ending index of p has been found
+            //However to make the algorithm output the starting index, length of p has to be subtracted
+            if(j == m-1){
+                ans.push_back(i-m+1);
+                //Change the starting point of comparison
                 j = pi[j];
             }
             else{
-                j++;
+                j += 1;
             }
         }
     }
