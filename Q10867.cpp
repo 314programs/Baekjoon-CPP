932e52289b470351e6b09e33f6a6adc4a20cd4d5
314programs <88335174+314programs@users.noreply.github.com>
2023-01-11 08:05:47 +0800

    Create Q10867.cpp

---
+++ Q10867.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+#define ll long long
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+    int n;
+    cin >> n;
+    set<int> a;
+
+    for(int i = 0; i < n; i++){
+        int num;
+        cin >> num;
+        a.insert(num);
+    }
+
+    for(auto ch: a){
+        cout << ch << " ";
+    }
+
+
+}
