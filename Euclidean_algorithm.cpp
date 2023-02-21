22126e5af9a8729ccfad19b5c2b33080ebffe34a
314programs <88335174+314programs@users.noreply.github.com>
2023-02-21 10:12:12 +0800

    Update Euclidean_algorithm.cpp

--- Euclidean_algorithm.cpp
+++ Euclidean_algorithm.cpp
@@ -7,3 +7,15 @@ int gcd(int a, int b){
         return gcd(b, a%b);
     }
 }
+
+//GCD algorithm for negative numbers that I found on stackoverflow
+//Thank you stackoverflow
+int gcd(int a, int b)
+{
+    if(a == 0) return b;
+    if(b == 0) return a;
+    a = abs(a);
+    b = abs(b);
+    if(a > b) return gcd(b, a);
+    return gcd(b%a, a);
+}
