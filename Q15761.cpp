5bd2b167c882158ffffbb41cd3293bba4862b44c
314programs <88335174+314programs@users.noreply.github.com>
2023-04-29 13:30:18 +0800

    Create Q15761.cpp

---
+++ Q15761.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+#define int long long
+
+void setIO(string s = ""){
+	if(s == "") return;
+	freopen((s+".in").c_str(), "r", stdin);
+	freopen((s+".out").c_str(), "w", stdout);
+}
+
+
+int32_t main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), std::cout.tie(NULL);
+
+	setIO("");
+
+	int n;
+	cin >> n;
+	vector<int> a(n);
+	for(int i = 0; i < n; i++){
+		cin >> a[i];
+	}
+	sort(a.begin(), a.end(), greater<int>());
+
+	int cnt = 0;
+	for(int i = 0; i < n; i++){
+		if(a[i] >= cnt){
+			cnt++;
+		}
+		else{
+			break;
+		}
+	}
+	cout << cnt;
+
+}
