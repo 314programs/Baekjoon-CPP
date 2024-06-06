9c300d32dcef8da12419adcbfe9e2266692f1094
314programs <88335174+314programs@users.noreply.github.com>
2024-06-06 10:57:32 +0800

    Create Q27328.cpp

---
+++ Q27328.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+#define int long long
+
+void setIO(string s = ""){
+	if(s == "") return;
+	freopen((s+".in").c_str(), "r", stdin);
+	freopen((s+".out").c_str(), "w", stdout);
+}
+
+int32_t main(){
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+	int a, b;
+	cin >> a >> b;
+	if(a < b) cout << -1;
+	else if(a == b) cout << 0;
+	else cout << 1;
+
+}
