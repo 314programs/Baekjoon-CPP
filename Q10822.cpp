27da1dbe59dc5fed04084e07a3a8eb54ab5cd51f
314programs <88335174+314programs@users.noreply.github.com>
2024-06-10 11:07:42 +0900

    Create Q10822.cpp

---
+++ Q10822.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+#define int long long
+
+void setIO(string s = ""){
+	if(s == "") return;
+	freopen((s+".in").c_str(), "r", stdin);
+	freopen((s+".out").c_str(), "w", stdout);
+}
+
+int32_t main(){
+ 
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL), cout.tie(NULL);
+
+	string s;
+	cin >> s;
+	string cur = "";
+	int ans = 0;
+	for(auto ch: s){
+		if(ch == ','){
+			ans += stoll(cur);
+			cur = "";
+		}
+		else{
+			cur += ch;
+		}
+	}
+	ans += stoll(cur);
+	cout << ans;
+
+}
