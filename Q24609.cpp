3b9ae5e921ba60250f7adf2eac04f8aa7ff34ab3
314programs <88335174+314programs@users.noreply.github.com>
2024-10-16 03:03:12 +0100

    Create Q24609.cpp

---
+++ Q24609.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+#define int long long
+
+void setIO(string s = ""){
+	if(s == "") return;
+	freopen((s+".in").c_str(), "r", stdin);
+	freopen((s+".out").c_str(), "w", stdout);
+}
+
+int32_t main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), std::cout.tie(NULL);
+
+	int n;
+	cin >> n;
+
+	int ans = 0;
+	int cur = 0;
+	for(int i = 0; i < n; i++){
+		int num;
+		cin >> num;
+		cur += num;
+		ans = min(ans, cur);
+	}
+	cout << abs(ans);
+
+}
