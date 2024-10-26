6335ac18405f1361f73b6e18d0727090a49915f7
314programs <88335174+314programs@users.noreply.github.com>
2024-10-26 04:12:09 +0100

    Create Q28808.cpp

---
+++ Q28808.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+using namespace std;
+#define int long long
+
+void setIO(string s = ""){
+	if(s == "") return;
+	freopen((s+".in").c_str(), "r", stdin);
+	freopen((s+".out").c_str(), "w", stdout);
+}
+
+int32_t main(){
+
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL), std::cout.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+	int ans = 0;
+	for(int i = 0; i < n; i++){
+		string s;
+		cin >> s;
+		for(int j = 0; j < m; j++){
+			if(s[j] == '+'){
+				ans++;
+				break;
+			}
+		}
+	}
+	cout << ans;
+
+}
